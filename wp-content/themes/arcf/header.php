084ff9318ae41b2b0777e7d85ede19aa0f3c5946
Wordpress User <lauren.maxwell@aarc.org>
2014-06-16 12:06:54 -0500

    Adds new archive file and adds it to the menu.

--- wp-content/themes/arcf/header.php
+++ wp-content/themes/arcf/header.php
@@ -71,9 +71,9 @@
 			<div class="menu-main-container" id="navigation">
 				<ul class="menu" id="dropmenu">
 					<li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-93" id="menu-item-93"><a href="/">Home</a></li>
-					<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-498" id="menu-item-498"><a href="gallery/oxygen/">Virtual Museum</a></li>
-					<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-502" id="menu-item-502"><a href="wall-of-donors/">Wall of Donors</a></li>
-					<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-94" id="menu-item-94"><a href="contact/">Contact</a></li>
+					<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-498" id="menu-item-498"><a href="/gallery/">Current Galleries</a></li>
+					<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-502" id="menu-item-502"><a href="/wall-of-donors/">Wall of Donors</a></li>
+					<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-94" id="menu-item-94"><a href="/contact/">Contact</a></li>
 				</ul>
 			</div>
 			<!--
