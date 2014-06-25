d642635bed3d6be2b5cfd24ce23bd68071ab5a69
Wordpress User <lauren.maxwell@aarc.org>
2014-06-25 11:29:51 -0500

    Updates URL for donate button.

--- wp-content/themes/arcf/menu.php
+++ wp-content/themes/arcf/menu.php
@@ -4,6 +4,6 @@
     <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-498" id="menu-item-498"><a href="/gallery/">Current Galleries</a></li>
     <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-502" id="menu-item-502"><a href="/wall-of-donors/">Wall of Donors</a></li>
     <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-94" id="menu-item-94"><a href="/contact/">Contact</a></li>
-    <li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item" id="menu-item-plugs"><a href="https://store.aarc.org/detail.aspx?id=BRICK">Donate</a></li>
+    <li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item" id="menu-item-plugs"><a href="https://store.aarc.org/SearchResults.aspx?searchterm=museum&searchoption=ALL">Donate</a></li>
   </ul>
 </div>
